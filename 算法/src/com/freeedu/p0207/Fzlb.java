e9bf9b58f42e114395189aaa150eb5e74377e7d4
LM <1151143484@qq.com>
2020-02-09 15:55:32 +0800

    链表两两反转

--- 算法/src/com/freeedu/p0207/Fzlb.java
+++ 算法/src/com/freeedu/p0207/Fzlb.java
@@ -1,7 +1,13 @@
 package com.freeedu.p0207;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 反转链表
+ * reverseList( 反转链表
+ * reverseBetween 反转指定位置m n之间的链表
+ * swapPairs（ 两两反转  1->2->3->4->null ==> 2->1->4->3
  * 
  * @author LM
  *
@@ -15,17 +21,52 @@ public class Fzlb {
 //			headt = headt.next;
 //		}
 		
-		ListNode head =  new ListNode(3);
-		head.next=new ListNode(5);
-		
-	
-		ListNode res = reverseBetween(head, 1,2);
-		while(res !=null) {
-			System.out.println(res.val);
-			res = res.next;
-		}
+//		ListNode head =  new ListNode(3);
+//		head.next=new ListNode(5);
+//		
+//	
+//		ListNode res = reverseBetween(head, 1,2);
+//		while(res !=null) {
+//			System.out.println(res.val);
+//			res = res.next;
+//		}
+		long l = 1581212791927L-1581212788829L;
+		System.out.println(l);
 		
 	}
+	
+	// 链表两两交换
+	public static ListNode swapPairs(ListNode head) {
+		// 如果链表是空 直接返回
+		if(head == null) return head;
+		// 交换第一个 
+		ListNode f = head;
+		// 交换第二个
+		ListNode s = head.next;
+		// 最后返回节点
+		ListNode res = head;
+		// 上一组最后一个节点
+		ListNode last = new ListNode(0);
+		if (s != null) {
+			res = s;
+		}
+		// 
+		while(f != null && s != null) {
+			// 把上一组末尾指针指向这一组的第二个节点（交换后第一个）
+			last.next = s;
+			// 交换位置
+			f.next = s.next;
+			s.next = f;
+			// 重新赋值 
+			last = f;
+			f = f.next;
+			s = f == null ? null : f.next;
+			
+		}
+		return res;
+	}
+
+
 	/**
 	 * 输入: 1->2->3->4->5->NULL 
 	 * 输出: 5->4->3->2->1->NULL
@@ -125,7 +166,60 @@ public class Fzlb {
 		
 		return fixed.next;
 	}
+	
+	
+	/**
+	 * 判断链表是否有环 
+	 * 暴力破解
+	 * @param head
+	 * @return
+	 */
+	public static  boolean   hascycle1(ListNode head) {
+		// 3000 给定一个时间
+		long start = System.currentTimeMillis();
+		while (head != null && (System.currentTimeMillis()-start) <3000) {
+			head = head.next;
+		}
+		if (head == null) return true;
+		return false;
+	}
+	
+	/**
+	 * 判断链表是否有环 
+	 * 用一个set把已经遍历的放进去 判断 如果下次放的时候已经存在 说明是环
+	 * @param head
+	 * @return
+	 */
+	public static  boolean   hascycle2(ListNode head) {
+		Set<ListNode> old = new HashSet<>();
+		while(head != null) {
+			if (old.contains(head)) return true;
+			old.add(head);
+			head = head.next;
+		}
+		return false;
+	}
+	/**
+	 * 判断链表是否有环 
+	 * 龟兔赛跑式 快指针 慢满指针 
+	 * @param head
+	 * @return
+	 */
+	public static  boolean   hascycle3(ListNode head) {
+		// 快指针
+		ListNode fast= head;
+		// 慢指针
+		ListNode slow = head ;
+		while(fast != null &&slow != null && fast.next !=null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) return true;
+		}
+		
+		return false;
+	}
 }
+
 
 class ListNode {
 	int val;
